eb4e9bd2b669689b2fed03397c1f5020a0532c14
Manzarshah1044 <153632083+Manzarshah1044@users.noreply.github.com>
2024-05-17 19:13:33 +0000

    Datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,31 @@
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "manzar" //string
+let age = 18    //number
+let isLoggedIn = false //boolean
+let state; //undefined
+
+// number => 2 to power 53 let a=453
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value let state=null
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof name);
