b35f59402da3ebae08fdd7ca6ba9a74f670e7e46
Manzarshah1044 <153632083+Manzarshah1044@users.noreply.github.com>
2024-05-18 10:04:28 +0000

    compare

---
+++ 01_Basics/04_Comparision.js
@@ -0,0 +1,18 @@
+console.log(4<5)
+console.log(3>=2)
+console.log(4==1)
+console.log("1"==1)  // only value cheak.. true ( bcz stringNumber automatic convert number)
+console.log("1"===1) //stricky equality  cheak value and datatype both ...false
+console.log("2">1)
+console.log("01">0)
+
+console.log(null>0)  //false
+console.log(null==0) //false
+console.log(null>=0) //true
+
+console.log(undefined==0) //false
+console.log(undefined>0)  //false
+console.log(undefined<0)  //false
+
+
+
